3ded2c16ef35bda8b88a95eeb4e854a885c92fd8
ShanigarapuVinay <vinay.shanigarapu@zemosolabs.com>
2024-07-01 14:41:54 +0530

    Initial Commit - Simple Component App

---
+++ component-app/src/components/Greetings.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Greetings() {
+  return (
+    <div>
+        <h1>Hello! Welcome to the React World, I hope you are having a wonderful day.</h1>
+    </div>
+  )
+}
